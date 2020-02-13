555033142a1eda7cde8d01a93e16e421eea1a6f3
EC2 Default User <ec2-user@ip-172-31-5-30.eu-central-1.compute.internal>
2020-02-13 05:50:05 +0000

    Amount of order data changed. Resource id start value changed.

--- templates/import_all.sh
+++ templates/import_all.sh
@@ -1,5 +1,5 @@
 mgeneratejs account_template.json -n 10000 | mongoimport --port 30001 --db crm --collection account &
 mgeneratejs customer_template.json -n 1000 | mongoimport --port 30001 --db crm --collection customer &
-mgeneratejs order_template.json -n 30000 | mongoimport --port 30001 --db crm --collection order &
+mgeneratejs order_template.json -n 10000 | mongoimport --port 30001 --db crm --collection order &
 mgeneratejs resource_template.json -n 5000000 | mongoimport --port 30001 --db crm --collection resource &
 mgeneratejs subscription_template.json -n 100000 | mongoimport --port 30001 --db crm --collection subscription &
